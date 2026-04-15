82493a50427742ea7a40f52b5db92a2be653fc15
Caleb Stimpson <caleb.stimpson@gmail.com>
2026-04-15 16:41:37 -0600

    Switch to System domain

--- src/Base12Calculator.hs
+++ src/Base12Calculator.hs
@@ -9,34 +9,21 @@ module Base12Calculator where
 import Clash.Prelude
 import Calculator.Prelude
 
--- Create a domain with the frequency of your input clock. For this example we used
--- 50 MHz.
-createDomain vSystem{vName="Dom50", vPeriod=hzToPeriod 50e6}
-
--- | @topEntity@ is Clash@s equivalent of @main@ in other programming languages.
--- Clash will look for it when compiling "Example.Project" and translate it to
--- HDL. While polymorphism can be used freely in Clash projects, a @topEntity@
--- must be monomorphic and must use non- recursive types. Or, to put it
--- hand-wavily, a @topEntity@ must be translatable to a static number of wires.
---
--- Top entities must be monomorphic, meaning we have to specify all type variables.
--- In this case, we are using the @Dom50@ domain, which we created with @createDomain@
--- and we are using 8-bit unsigned numbers.
 topEntity ::
-  Clock Dom50 ->
-  Reset Dom50 ->
-  Enable Dom50 ->
-  Signal Dom50 Bit ->
-  Signal Dom50 Bit ->
-  Signal Dom50 Bit ->
-  Signal Dom50 Bit ->
-  Signal Dom50 Bit ->
-  Signal Dom50 Bit ->
-  Signal Dom50 (Unsigned 4) ->
-  (Signal Dom50 (Unsigned 7),
-  Signal Dom50 (Unsigned 16),
-  Signal Dom50 (Unsigned 4),
-  Signal Dom50 (Unsigned 4))
+  Clock System ->
+  Reset System ->
+  Enable System ->
+  Signal System Bit ->
+  Signal System Bit ->
+  Signal System Bit ->
+  Signal System Bit ->
+  Signal System Bit ->
+  Signal System Bit ->
+  Signal System (Unsigned 4) ->
+  (Signal System (Unsigned 7),
+  Signal System (Unsigned 16),
+  Signal System (Unsigned 4),
+  Signal System (Unsigned 4))
 topEntity = exposeClockResetEnable accum
 
 -- To specify the names of the ports of our top entity, we create a @Synthesize@ annotation.
